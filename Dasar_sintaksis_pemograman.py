b59f463ac1d4cbd76ff9ab003ba0d084a187c5d2
mochfauzi573 <mochfauzi573@gmail.com>
2022-01-12 12:04:40 +0700

    dasar sintaksis pemograman

---
+++ Dasar_sintaksis_pemograman.py
@@ -0,0 +1,19 @@
+# Percabangan
+Milk_Bottle_account = 987
+egg_Account = 1755
+print(f"Jumlah botol susu {Milk_Bottle_account} botol")
+print(f"Jumlah telor {egg_Account} butir")
+
+
+if Milk_Bottle_account > 10:
+
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    if egg_Account > 0:
+        print("Budi membeli 1 botol susu")
+    else:
+        print("Budi tidak jadi membeli 1 botol susu")
+else:
+    print("budi tidak jadi membeli 1 botol susu")
+
+    print("budi pulang ke rumah")
+    print("menyerahkan hasilnya kepada ibu")
