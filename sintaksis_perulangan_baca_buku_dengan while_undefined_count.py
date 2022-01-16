5b51c9a62328e62925a7d39bffd5d8cf6efe6d90
mochfauzi573 <mochfauzi573@gmail.com>
2022-01-16 09:37:05 +0700

    contoh studi kasus dengan for pada pemograman python

---
+++ sintaksis_perulangan_baca_buku_dengan while_undefined_count.py
@@ -0,0 +1,29 @@
+"""
+program perulangan membaca buku dengan while sampai paham
+"""
+
+jumlah_buku = 10
+print('Ibu berkata, "Baca semua bukumu')
+total_jumlah_baca = 0
+
+
+jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
+print(f'Jumlah buku yang sudah dibaca dan di pahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
+
+
+while total_jumlah_baca < jumlah_buku * 2:
+    total_jumlah_baca = total_jumlah_baca + 1
+    if jumlah_buku_yang_sudah_dibaca_dan_dipahami == 9:
+        print(f"Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1} belum paham")
+
+    else:
+        jumlah_buku_yang_sudah_dibaca_dan_dipahami = jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1
+        print(f"Baca ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} sudah di baca dan di pahami")
+
+     #print(f'Jumlah buku yang sudah dibaca dan di pahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
+    if jumlah_buku_yang_sudah_dibaca_dan_dipahami == jumlah_buku:
+        print('"Bu, semua buku sudah dibaca dan pahami')
+    else:
+        print(f'"Bu, tidak semua buku bisa di pahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami} buku')
+
+
