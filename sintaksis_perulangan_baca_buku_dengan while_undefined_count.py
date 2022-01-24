d839652a94fa96da6d9bc4267d9cec19c80b8285
mochfauzi573 <mochfauzi573@gmail.com>
2022-01-24 10:27:03 +0700

    rename variabel in english

--- sintaksis_perulangan_baca_buku_dengan while_undefined_count.py
+++ sintaksis_perulangan_baca_buku_dengan while_undefined_count.py
@@ -2,28 +2,28 @@
 program perulangan membaca buku dengan while sampai paham studi kasus nyata
 """
 
-jumlah_buku = 10
+book_count = 10
 print('Ibu berkata, "Baca semua bukumu')
-jumlah_baca = 0
+read_count = 0
 
 
-jumlah_paham = 0
-print(f'Jumlah buku yang sudah dibaca dan di pahami {jumlah_paham}')
+understood_count = 0
+print(f'Jumlah buku yang sudah dibaca dan di pahami {understood_count}')
 
 
-while jumlah_baca < jumlah_buku * 2:
-    jumlah_baca = jumlah_baca + 1
-    if jumlah_paham == 9:
-        print(f"Buku ke {jumlah_paham + 1} belum paham")
+while read_count < book_count * 2:
+    read_count = read_count + 1
+    if understood_count == 9:
+        print(f"Buku ke {understood_count + 1} belum paham")
 
     else:
-        jumlah_paham = jumlah_paham + 1
-        print(f"Baca ke {jumlah_paham} sudah di baca dan di pahami")
+        understood_count = understood_count + 1
+        print(f"Baca ke {understood_count} sudah di baca dan di pahami")
 
      #print(f'Jumlah buku yang sudah dibaca dan di pahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
-    if jumlah_paham == jumlah_buku:
+    if understood_count == book_count:
         print('"Bu, semua buku sudah dibaca dan pahami')
     else:
-        print(f'"Bu, tidak semua buku bisa di pahami {jumlah_paham} buku')
+        print(f'"Bu, tidak semua buku bisa di pahami {understood_count} buku')
 
 
