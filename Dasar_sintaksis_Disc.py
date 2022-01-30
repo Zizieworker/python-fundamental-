e27b0b5f4c1bb818acdc22edc574147db146892d
mochfauzi573 <mochfauzi573@gmail.com>
2022-01-30 10:46:11 +0700

    Dasar syntax dictionary pada python

--- Dasar_sintaksis_Disc.py
+++ Dasar_sintaksis_Disc.py
@@ -1,12 +1,31 @@
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
+users = {
+
+  "Id": "1",
+  "name": "Mustang",
+  "username": "John george",
+  "email" : "berniagakuh@gmail.com",
+  "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+    "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+          }
+ }
 }
-print(thisdict)
-print(thisdict["brand"])
-print(thisdict["model"])
-print(thisdict["year"])
+print(users)
+print(users["Id"])
+print(users["name"])
+print(users["username"])
+print(users["email"])
+print(users["address"]["street"])
+print(users["address"]["suite"])
+print(users["address"]["geo"]["lat"])
+
+
+
+
 
 
 
